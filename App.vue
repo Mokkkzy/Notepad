6e65a6036cd8bdcd66a0f25e9d3945a4920a2f96
你好2007 <2501482523@qq.com>
2021-08-23 10:08:37 +0800

    修改启动配置,加速启动

--- App.vue
+++ App.vue
@@ -1,13 +1,13 @@
 <script>
     export default {
         onLaunch: function() {
-            console.log('App Launch')
+            plus.navigator.closeSplashscreen()
         },
         onShow: function() {
-            console.log('App Show')
+
         },
         onHide: function() {
-            console.log('App Hide')
+
         }
     }
 </script>
